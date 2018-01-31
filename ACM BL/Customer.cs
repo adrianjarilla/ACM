210d193c297bfa9b8afd375e679cbf981674ccc8
Adrian Jarilla <adrian_jarilla@yahoo.com>
2018-02-01 03:50:37 +0800

    Building Methods for Customer Class

--- ACM BL/Customer.cs
+++ ACM BL/Customer.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ACM_BL
 {
     public class Customer
     {
+        public static int InstanceCount { get; set; }
         private string _lastName;
         
         public string LastName
@@ -43,6 +45,43 @@ namespace ACM_BL
 
                 return fullName;
             }
+        }
+
+        /// <summary>
+        /// Retrieve one customer
+        /// </summary>
+        /// <param name="customerId"></param>
+        /// <returns></returns>
+        public Customer Retrieve(int customerId)
+        {
+            return new Customer();
+        }
+
+        /// <summary>
+        /// Retrieve
+        /// </summary>
+        /// <returns></returns>
+        public List<Customer> Retrieve()
+        {
+            return new List<Customer>();
+        }
+
+        /// <summary>
+        /// Saves the current customer
+        /// </summary>
+        /// <returns></returns>
+        public bool Save()
+        {
+            return true;
+        }
+
+        public bool Validate()
+        {
+            var isValid = true;
+            if (string.IsNullOrWhiteSpace(LastName)) isValid = false;
+            if (string.IsNullOrWhiteSpace(EmailAddress)) isValid = false;
+
+            return isValid;
         }
 
 
